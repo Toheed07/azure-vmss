3dc4b084be5eb4aa823a942cd29ea5ab54307a19
Toheed-PanduAI <toheed.jamaal@pandu.ai>
2024-09-27 10:39:53 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import uvicorn
+import httpx
+from fastapi import (
+    FastAPI,
+    Response,
+    Query,
+    Request,
+    HTTPException,
+    Depends,
+    UploadFile,
+    File,
+    Form,
+    BackgroundTasks,
+    Body,
+    Body,
+)
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import StreamingResponse, JSONResponse, RedirectResponse
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="PanduAI Backend", version="0.1.0")
+
+
+@app.get("/")
+def health_check():
+    return {"message": "welcome"}
+
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+
+
+app = CORSMiddleware(
+    app=app,
+    allow_origins=[
+        "https://app.pandu.ai/",
+        "https://app.pandu.ai",
+        "http://localhost:3000",
+        "http://localhost:3000/",
+        "http://localhost:8000",
+        "http://localhost:8000/",
+        "https://app-new.pandu.ai",
+        "https://app-new.pandu.ai/",
+    ],
+    allow_credentials=True,
+    # allow_methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
+    # allow_headers=["Content-Type"] + get_all_cors_headers(),
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
